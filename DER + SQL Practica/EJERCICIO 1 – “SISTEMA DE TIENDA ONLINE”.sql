d514ac8555137328c5fc3667cd5b1fda08b5b7fd
pikitoria <anaiscalderonacurero@gmail.com>
2026-05-21 12:49:55 -0300

    Update EJERCICIO 1 – “SISTEMA DE TIENDA ONLINE”.sql

--- DER + SQL Practica/EJERCICIO 1 – “SISTEMA DE TIENDA ONLINE”.sql
+++ DER + SQL Practica/EJERCICIO 1 – “SISTEMA DE TIENDA ONLINE”.sql
@@ -1,30 +1,31 @@
-CREATE DATABASE CLINICA;
-USE CLINICA;
+CREATE DATABASE TIENDA;
+USE TIENDA;
 
-CREATE TABLE ESPECIALIDADES (
-    id_especialidad INT AUTO_INCREMENT PRIMARY KEY,
-    nombre_esp VARCHAR(50) NOT NULL
+CREATE TABLE ROPA (
+    id_ropa INT AUTO_INCREMENT PRIMARY KEY,
+    prenda VARCHAR(50) NOT NULL,
+    precio DECIMAL(10,2) NOT NULL
 );
 
-CREATE TABLE MEDICOS (
-    id_medico INT AUTO_INCREMENT PRIMARY KEY,
-    nombre VARCHAR(100) NOT NULL,
-    id_especialidad INT,
-    FOREIGN KEY (id_especialidad) REFERENCES ESPECIALIDADES(id_especialidad)
+CREATE TABLE VENDEDORES (
+    id_vendedor INT AUTO_INCREMENT PRIMARY KEY,
+    nombre VARCHAR(50) NOT NULL
 );
 
-CREATE TABLE TURNOS (
-    id_turno INT AUTO_INCREMENT PRIMARY KEY,
-    fecha DATE NOT NULL,
-    estado ENUM('Confirmado', 'Cancelado', 'Realizado'),
-    id_medico INT,
-    FOREIGN KEY (id_medico) REFERENCES MEDICOS(id_medico)
+CREATE TABLE VENTAS (
+    id_venta INT AUTO_INCREMENT PRIMARY KEY,
+    id_ropa INT,
+    id_vendedor INT,
+    estado_pedido ENUM('Pendiente', 'Enviado', 'Entregado'),
+    FOREIGN KEY (id_ropa) REFERENCES ROPA(id_ropa),
+    FOREIGN KEY (id_vendedor) REFERENCES VENDEDORES(id_vendedor)
 );
 
-INSERT INTO ESPECIALIDADES (nombre_esp) VALUES ('Pediatría'), ('Cardiología'), ('Traumatología');
-INSERT INTO MEDICOS (nombre, id_especialidad) VALUES ('Dr. House', 3), ('Dra. Grey', 2), ('Dr. Meléndez', 1);
-INSERT INTO TURNOS (fecha, estado, id_medico) VALUES ('2024-06-01', 'Confirmado', 1), ('2024-06-02', 'Cancelado', 2), ('2024-06-03', 'Realizado', 3);
 
-SELECT M.nombre, E.nombre_esp FROM MEDICOS M JOIN ESPECIALIDADES E ON M.id_especialidad = E.id_especialidad;
-SELECT * FROM TURNOS;
+INSERT INTO ROPA (prenda, precio) VALUES ('Remera', 1500), ('Pantalón', 3000), ('Buzo', 4500);
+INSERT INTO VENDEDORES (nombre) VALUES ('Marcos'), ('Julia'), ('Roberto');
+INSERT INTO VENTAS (id_ropa, id_vendedor, estado_pedido) VALUES (1, 2, 'Entregado'), (2, 1, 'Pendiente'), (3, 3, 'Enviado');
 
+SELECT prenda, precio FROM ROPA;
+
+SELECT V.id_venta, VEN.nombre FROM VENTAS V JOIN VENDEDORES VEN ON V.id_vendedor = VEN.id_vendedor;
