da4f69e5933ece29b7472d33665221cac18f066a
pikitoria <anaiscalderonacurero@gmail.com>
2026-06-08 09:54:05 -0300

    Update Gestión de gimnasio.sql

--- Inner joins dificiles/Gimnasio/Gestión de gimnasio.sql
+++ Inner joins dificiles/Gimnasio/Gestión de gimnasio.sql
@@ -28,3 +28,20 @@ INNER JOIN PAGOS P ON S.id_socio = P.id_socio
 GROUP BY S.id_socio
 HAVING SUM(P.monto) > (SELECT costo FROM PLANES WHERE tipo = 'Premium'); 
 
+SELECT PL.nombre, SUM(PL.tipo) AS RecaudacionTotaL
+FROM PLANES PL
+INNER JOIN SOCIOS S ON PLA.id_plan=  S.id_plan
+INNER JOIN PAGOS PA  ON S.id_socio = PA.id_socio
+WHERE EX.estado = 'Pago'
+GROUP BY E.id_edif
+HAVING AVG(EX.monto) >= 20000;
+
+SELECT E.nombre, SUM(EX.monto) AS RecaudacionReal
+FROM EDIFICIOS E
+INNER JOIN UNIDADES U ON E.id_edif = U.id_edif
+INNER JOIN EXPENSAS EX ON U.id_unit = EX.id_unit
+WHERE EX.estado = 'Pago'
+GROUP BY E.id_edif
+ORDER BY RecaudacionReal DESC 
+LIMIT 3;
+
