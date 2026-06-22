8dd9cf3e6a56e213ffb62bf49e92848eb8b128c3
pikitoria <anaiscalderonacurero@gmail.com>
2026-06-22 09:23:29 -0300

    Update concesionario.sql

--- ACT3 Concesionario/concesionario.sql
+++ ACT3 Concesionario/concesionario.sql
@@ -2,7 +2,7 @@ CREATE DATABASE CONCESIONARIO;
 USE CONCESIONARIO;
 
 CREATE TABLE CLIENTES (
-    dni INT PRIMARY KEY, -- DNI como PK fija según video [5]
+    dni INT PRIMARY KEY,
     nombre VARCHAR(50) NOT NULL,
     apellidos VARCHAR(50) NOT NULL,
     direccion VARCHAR(100),
@@ -15,9 +15,9 @@ CREATE TABLE COCHES (
     modelo VARCHAR(50),
     marca VARCHAR(50),
     color VARCHAR(20),
-    tipo ENUM('Nuevo', 'Usado'), -- Uso de ENUM según teoría [3]
-    unidades INT DEFAULT 0, -- Para coches nuevos
-    kilometros INT DEFAULT 0, -- Para coches usados
+    tipo ENUM('Nuevo', 'Usado'), 
+    unidades INT DEFAULT 0,
+    kilometros INT DEFAULT 0, 
     dni_cliente INT,
     FOREIGN KEY (dni_cliente) REFERENCES CLIENTES(dni)
 );
@@ -27,7 +27,7 @@ CREATE TABLE MECANICOS (
     nombre VARCHAR(50),
     apellidos VARCHAR(50),
     fecha_contratacion DATE,
-    salario DECIMAL(10, 2) -- Decimal para dinero [3]
+    salario DECIMAL(10, 2) 
 );
 
 CREATE TABLE REPARACIONES (
@@ -42,10 +42,22 @@ CREATE TABLE REPARACIONES (
 
 
 INSERT INTO CLIENTES (dni, nombre, apellidos, direccion, telefono) VALUES 
-(10, 'Carlos', 'Gomez', 'Av 1', '111'), (20, 'Lucia', 'Paz', 'Av 2', '222'),
-(30, 'Marcos', 'Sosa', 'Av 3', '333'), (40, 'Elena', 'Ruiz', 'Av 4', '444'),
-(50, 'Pedro', 'Díaz', 'Av 5', '555');
+('10', 'Carlos', 'Gomez', 'Av 1', '111'), ('20', 'Lucia', 'Paz', 'Av 2', '222'),
+('30', 'Marcos', 'Sosa', 'Av 3', '333'), ('40', 'Elena', 'Ruiz', 'Av 4', '444'),
+('50', 'Pedro', 'Díaz', 'Av 5', '555');
 
 INSERT INTO COCHES (matricula, modelo, marca, color, tipo, kilometros, dni_cliente) VALUES 
-('AAA111', 'Fiesta', 'Ford', 'Azul', 'Usado', 50000, 10),
-('BBB222', 'Corolla', 'Toyota', 'Blanco', 'Nuevo', 0, 20)
+('AAA111', 'Territory', 'Ford', 'Negro', 'Nuevo', '20', '95752916'), ('BBB222', 'Corolla', 'Toyota', 'Blanco', 'Nuevo', '0', '39859485'),
+('AA1506', 'Veneno', 'Lamborghini', 'Negro', 'Nuevo', '20' , '95573013'), ('WEBO15', 'RS6', 'Mercedes', 'Negro', 'Nuevo', '0', '94854903'),
+('BBB222', 'Hot wheels', 'Toyota', 'Rosa fluor', 'Nuevo', '0', '49761533');
+
+
+INSERT INTO MECANICOS ( dni, nombre, apellidos, fecha_contratacion, salario) VALUES
+('10989574', 'Carlos', 'Gomez', '23-08-1998', '4'), ('20345634', 'Lucia', 'Paz', 'Av 2', '20000'),
+('30256634', 'Marcos', 'Sosa', 'Av 3', '2'), ('40564535', 'Elena', 'Luanez', 'Av 4', '40000'),
+('50565423', 'Pedro', 'Díaz', 'Av 5', '3');
+
+INSERT INTO REPARACIONES (dni_mecanico, id_coche, fecha_reparacion, horas_trabajo) VALUES
+('10', 'Carlos', 'Gomez', '2-4-2026', '78'), ('20', 'Lucia', 'Paz', '23-10-2025', '2'),
+('30', 'Marcos', 'Sosa', '15-06-2026', '23'), ('40', 'Elena', 'Luanez', '07-8-2005', '4'),
+('50', 'Pedro', 'Díaz', '06-07-2025', '24');
